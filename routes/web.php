47ef4b5a45e2fa295a493e22334f8821cdb9648a
Laila Asrin <laila_as@mhs.unsyiah.ac.id>
2023-04-13 14:07:15 +0700

    feat : modify web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegisterController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'prevent-back-history'], function () {
+    // Route ini akan mengarahkan ke halaman utama
+// url: yang akhiranya adalah / akan diarahkan ke halaman utama
+    Route::get('/', [PagesController::class, 'halamanUtama']);
+
+
+    // Route ini akan mengarahkan ke halaman login
+// url: yang akhiranya adalah /loginpage akan diarahkan ke halaman login
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+
+
+    // Route ini akan mengarahkan ke halaman register
+// url: yang akhiranya adalah /registerPage akan diarahkan ke halaman register
+    Route::get('/register', [RegisterController::class, 'index']);
+
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/register', [RegisterController::class, 'store']);
+
+
+    // Jika session login tidak ada maka akan diarahkan ke halaman login page dan jika ada maka akan diarahkan ke halaman dashboard
+// Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
+// Route::get('/myprofile', [PagesController::class, 'myprofile'])->middleware('auth');
+// Route::get('/ongoing', [PagesController::class, 'ongoing'])->middleware('auth');
+// Route::get('/history', [PagesController::class, 'history'])->middleware('auth');
+
+    Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
+    Route::get('/myprofile', [PagesController::class, 'myprofile'])->middleware('auth');
+    Route::get('/ongoing', [PagesController::class, 'ongoing'])->middleware('auth');
+    Route::get('/history', [PagesController::class, 'history'])->middleware('auth');
+    Route::get('/logout', [LoginController::class, 'destroy'])->middleware('auth');
+    Route::get('/pickup', [PagesController::class, 'pickup'])->middleware('auth');
+    Route::get('/informasi', [PagesController::class, 'informasi'])->middleware('auth');
+
+
+
+
+    Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
+    Route::get('/myprofile', [PagesController::class, 'myprofile'])->middleware('auth');
+    Route::get('/ongoing', [PagesController::class, 'ongoing'])->middleware('auth');
+    Route::get('/history', [PagesController::class, 'history'])->middleware('auth');
+    Route::get('/exchange', [PagesController::class, 'exchange'])->middleware('auth');
+    Route::get('/tukar', [PagesController::class, 'tukar'])->middleware('auth');
+
+
+    // Route ini akan mengarahkan ke halaman logout
+// url: yang akhiranya adalah /logout akan diarahkan ke halaman logout
+// jika user sudah logout maka akan diarahkan ke halaman login dan session akan berhenti
+
 });
