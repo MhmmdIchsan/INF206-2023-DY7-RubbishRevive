63d6859d468c5f80ef3240660524d31f5e86dc91
Trixie28 <anrean@mhs.unsyiah.ac.id>
2023-04-14 02:19:31 +0700

    feat: add configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
