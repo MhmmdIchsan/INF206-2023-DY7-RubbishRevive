04009b62a80dc94d611078a70fae0e82309d9203
MhmmdIchsan <mhichsan21@mhs.unsyiah.ac.id>
2023-04-27 20:30:16 +0700

    docs: add guest if-else function for 'navbar'

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -160,9 +160,32 @@
 </header>
 
 @endauth
+@else
+<header class="relative inset-x-0 bottom-0">
+    <div class="flex inset-x-0 relative w-auto relative bg-[#A7D1DD] flex justify-between shadow-xl">
+        <div class="px-4 flex justify-start">
+            <a href="/dashboard" class="font-bold text-lg text-primary block py-1">
+                <img src="{{ asset('image/logo.png') }}" alt="logo" class="w-20">
+            </a>
+
         </div>
-    </div>
+        <div class="flex items-center px-4 ">
+            <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden ">
+                <span class="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+            </button>
+        </div>
+        <div class="flex justify-end">
+            <div class="group flex items-center">
+                <a href="/login" class="text-base text-black py-2 mx-8 flex group-hover:text-primary">Login</a>
+            </div>
+            <div class="group flex items-center">
+                <a href="/register" class="text-base text-black py-2 mx-8 flex group-hover:text-primary">Register</a>
+            </div>
+        </div>
     </div>
 
     </div>
 </header>
+@endif
