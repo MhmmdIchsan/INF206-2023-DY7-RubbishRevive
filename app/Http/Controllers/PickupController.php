6e9b1d602e4784ea8f45a5f17512c90f8d3b2f5e
MhmmdIchsan <mhichsan21@mhs.unsyiah.ac.id>
2023-05-05 03:02:25 +0700

    feat: add PickupController

---
+++ app/Http/Controllers/PickupController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pickup;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class PickupController extends Controller
+{
+    
+}
