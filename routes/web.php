81df53785f24d2047657b9bf103ed48bd58218ba
MhmmdIchsan <mhichsan21@mhs.unsyiah.ac.id>
2023-05-12 08:18:10 +0700

    fix: fixing route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\auth\RegisterController;
 use App\Http\Controllers\PickupController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TukarPoinController;
+
 
 
 
@@ -40,8 +42,15 @@ Route::group(['middleware' => 'prevent-back-history'], function () {
     Route::get('/login', [LoginController::class, 'index'])->name('login');
 
     // Pickup Start Here
-    Route::resource('/ongoing', PickupController::class);
     Route::resource('/pickup', PickupController::class);
+    Route::resource('/ongoing', PickupController::class);
+    Route::get('pickup', [PickupController::class, 'index'])->name('pickup.index');
+    Route::get('ongoing', [PickupController::class, 'ongoing'])->name('pickup.ongoing');
+    Route::get('price', [PickupController::class, 'getPrice'])->name('pickup.getPrice');
+    Route::get('pickup.history', [PickupController::class, 'history'])->name('pickup.history');
+    
+    // make route for change status
+
     // Pickup End Here
 
     // Route ini akan mengarahkan ke halaman register
@@ -51,28 +60,18 @@ Route::group(['middleware' => 'prevent-back-history'], function () {
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/register', [RegisterController::class, 'store']);
 
-
-    // Jika session login tidak ada maka akan diarahkan ke halaman login page dan jika ada maka akan diarahkan ke halaman dashboard
-// Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
-// Route::get('/myprofile', [PagesController::class, 'myprofile'])->middleware('auth');
-// Route::get('/ongoing', [PagesController::class, 'ongoing'])->middleware('auth');
-// Route::get('/history', [PagesController::class, 'history'])->middleware('auth');
-
     Route::get('/dashboard', [PagesController::class, 'dashboard'])->middleware('auth');
     Route::get('/myprofile', [PagesController::class, 'myprofile'])->middleware('auth');
-    // Route::get('/ongoing', [PagesController::class, 'ongoing'])->middleware('auth');
     Route::get('/history', [PagesController::class, 'history'])->middleware('auth');
     Route::get('/logout', [LoginController::class, 'destroy'])->middleware('auth');
-    // Route::get('/pickupp', [PagesController::class, 'pickup'])->middleware('auth');
     Route::get('/exchange', [PagesController::class, 'exchange'])->middleware('auth');
-    Route::get('/tukarPulsa10', [PagesController::class, 'tukarPulsa10'])->middleware('auth');
-    Route::get('/tukarPulsa20', [PagesController::class, 'tukarPulsa20'])->middleware('auth');
-    Route::get('/tukarPulsa50', [PagesController::class, 'tukarPulsa50'])->middleware('auth');
-    Route::get('/tukarPulsa100', [PagesController::class, 'tukarPulsa100'])->middleware('auth');
-    Route::get('/tukarListrik50', [PagesController::class, 'tukarListrik50'])->middleware('auth');
-    Route::get('/tukarListrik100', [PagesController::class, 'tukarListrik100'])->middleware('auth');
-    Route::get('/tukarListrik150', [PagesController::class, 'tukarListrik150'])->middleware('auth');
-    Route::get('/tukarListrik200', [PagesController::class, 'tukarListrik200'])->middleware('auth');
+
+    // Penukaran Start Here
+    Route::resource('/tukarpoin', TukarPoinController::class)->middleware('auth');
+    Route::get('/tukarPoin', [TukarPoinController::class, 'create'])->middleware('auth');
+    // Penukaran End Here
+
+
     Route::get('/aboutus', [PagesController::class, 'aboutus'])->middleware('auth');
     Route::get('/pricelist', [PagesController::class, 'pricelist'])->middleware('auth');
     Route::get('/jadwal-pickup', [PagesController::class, 'jadwalPickup'])->middleware('auth');
@@ -80,8 +79,4 @@ Route::group(['middleware' => 'prevent-back-history'], function () {
     // Admin
     Route::resource('/manageusers', UserController::class);
     // Admin End here
-    // Route ini akan mengarahkan ke halaman logout
-// url: yang akhiranya adalah /logout akan diarahkan ke halaman logout
-// jika user sudah logout maka akan diarahkan ke halaman login dan session akan berhenti
-
 });
