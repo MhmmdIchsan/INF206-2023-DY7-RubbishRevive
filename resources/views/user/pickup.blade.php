58a1118cf8408deaab9bc0c85ee02f379b258fce
Nabilah Qurratul Annisa <nabilah821@mhs.unsyiah.ac.id>
2023-05-12 07:23:37 +0700

    docs: modify pickup user

--- resources/views/user/pickup.blade.php
+++ resources/views/user/pickup.blade.php
@@ -6,75 +6,79 @@ class="bg-[#D6EEF5]"
 @endsection
 
 @section('content')
-<!-- make form -->
-<div class="sm:mx-auto sm:w-full sm:max-w-md">
-    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-        Isi Form
-    </h2>
-</div>
 
-<div>
-    <div class="mt-8 sm:mx-auto sm:max-w-md">
-        <form class="space-y-6" action="{{ route('pickup.store') }}" method="POST" enctype="multipart/form-data" class="flex justify-center">
-        @csrf
-            <div>
-                <div class="mt-1 pb-2">
-                    <input id="nama" name="nama" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Nama">
-                </div>
-                <div class="mt-1 pb-2">
-                    <input id="alamat" name="alamat" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Alamat">
-                </div>
-                <div class="mt-1 pb-2">
-                    <input id="phone" name="phone" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Nomor Hp">
-                </div>
-                <div class="mt-1 pb-2">
-                    <input id="email" name="email" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Email">
-                </div>
-                <div class="mt-1 pb-2">
-                    <input id="jenissampah" name="jenissampah" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Jenis Sampah">
-                </div>
-                <div class="mt-1 pb-2">
-                    <input id="beratsampah" name="beratsampah" required
-                        class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                        placeholder="Berat Sampah">
-                </div>
-                <div class="form-group">
-                    <label class="font-weight-bold">GAMBAR</label>
-                    <input id="image" type="file" class="form-control @error('image') is-invalid @enderror"
-                        name="image">
+<section id="home" class="pt-20 ">
+    <div class="container content-center')">
+        <div class="flex flex-wrap ">
+            <div class="w-full self-center px-4 text-center ">
+                
+                <h1 class="block font-normal text-slate-800 text-5xl mt-1 lg:text-5xl text-[#9ADCF1]">Pick-up Request</h1>
+                <div class="card border-0 shadow-sm rounded">
 
-                    <!-- error message untuk title -->
-                    @error('image')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
                 </div>
-                <div>
-                </div>
-                <div class="flex justify-center">
-                    <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
-                </div>
-
             </div>
-        </form>
+        </div>
+    </div>
+</section>
+<div class="flex flex-warp justify-center">
+    <div class="overflow-x-auto">
+        <div class="p-1.5 min-w-full inline-block align-middle">
+            <div class="overflow-hidden">
+                <div class="flex justify-center pt-5">
+                    <a href="{{ route('pickup.create') }}"
+                        class="btn btn-md btn-success text-white m-2 p-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Request
+                        Pickup</a>
+                </div>
+                @forelse ($pickups as $pickup)
+                <div
+                    class="flex flex-col mb-5 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+                    <div class="m-5">
+                    @if ($pickup->image)
+                    <img src="{{ asset ('storage/' . $pickup->image) }}" alt="image" class="w-25 h-25 rounded-lg">
+                    @else
+                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                        src="/docs/images/blog/image-4.jpg" alt="">
+                    @endif
+                    </div>
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ $pickup->nama }}</h5>
+                        <h6 class="mb-2 text-xl tracking-tight text-gray-900">{{ $pickup->status }}</h6>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $pickup->alamat }}</p>
+                    </div>
+                </div>
+                <!-- make button to show harga -->
+                @empty
+                <div class="alert alert-danger">
+                    Data Request Pickup belum Tersedia.
+                </div>
+                @endforelse
+                {{ $pickups->links() }}
+            </div>
+        </div>
     </div>
 </div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 <script>
-    CKEDITOR.replace('content');
+    //message with toastr
+    @if(session() -> has('success'))
+
+    toastr.success('{{ session('
+        success ') }}', 'BERHASIL!');
+
+    @elseif(session() -> has('error'))
+
+    toastr.error('{{ session('
+        error ') }}', 'GAGAL!');
+
+    @endif
 
 </script>
+<!-- End Hero Section -->
+
+
+
 @endsection
