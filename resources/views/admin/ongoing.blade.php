4c9299aa6b326a5e9b8091a4152a62d02bb14cdb
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2023-12-27 19:55:22 +0700

    feat: add toastr to exchange feature

--- resources/views/admin/ongoing.blade.php
+++ resources/views/admin/ongoing.blade.php
@@ -2,13 +2,13 @@
 
 
 @section('body')
-class="bg-[#D6EEF5]"
+class="bg-gradient-to-tl from-cyan-500"
 @endsection
 
 @section('content')
 
 <section id="home" class="pt-20 ">
-    <div class="container content-center')">
+    <div class="container content-center mx-auto">
         <div class="flex flex-wrap ">
             <div class="w-full self-center px-4 text-center ">
                 <ul class="lg:flex justify-center">
@@ -17,7 +17,8 @@ class="bg-[#D6EEF5]"
                                 src="{{ asset('image/ongoing.png') }}" class="scale-100"></img></a>
                     </li>
                 </ul>
-                <h1 class="block font-normal text-slate-800 text-5xl mt-1 lg:text-5xl text-[#9ADCF1] underline py-3">On-Going
+                <h1 class="block font-normal text-slate-800 text-5xl mt-1 lg:text-5xl text-[#9ADCF1] underline py-3">
+                    On-Going
                     Transaction</h1>
                 <div class="card border-0 shadow-sm rounded">
 
@@ -30,7 +31,7 @@ class="bg-[#D6EEF5]"
     <div class="-m-1.5 overflow-x-auto">
         <div class="p-1.5 min-w-full inline-block align-middle">
             <div class="overflow-hidden">
-                
+
                 @forelse ($pickups as $pickup)
                 <div
                     class="flex flex-col mb-5 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
@@ -40,11 +41,12 @@ class="bg-[#D6EEF5]"
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ $pickup->nama }}</h5>
                         <h6 class="mb-2 text-xl tracking-tight text-gray-900">{{ $pickup->status }}</h6>
                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $pickup->alamat }}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Driver : {{ $pickup->driver_id }} - {{ $pickup->driver_name }}</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Driver : {{ $pickup->driver_id }} -
+                            {{ $pickup->driver_name }}</p>
                     </div>
                 </div>
                 @empty
-                <div class="alert alert-danger">
+                <div class="alert alert-danger text-center">
                     Data Request Pickup belum Tersedia.
                 </div>
                 @endforelse
@@ -62,42 +64,43 @@ class="bg-[#D6EEF5]"
                     </div>
                     <div class="items-center px-5">
                         <form action="{{ route('pickup.destroy',$pickup->id) }}" method="POST">
-                            <a class="btn btn-primary text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="{{ route('tukarpoin.edit',$pickup->id) }}">Terima</a>
+                            <a class="btn btn-primary text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                href="{{ route('tukarpoin.edit',$pickup->id) }}">Terima</a>
                         </form>
                     </div>
                 </div>
-                
+
                 @empty
                 <div class="alert alert-danger">
-                    
-                @endforelse
-                
+
+                    @endforelse
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-<script>
-    //message with toastr
-    @if(session() -> has('success'))
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-    toastr.success('{{ session('
-        success ') }}', 'BERHASIL!');
+    <script>
+        $(document).ready(function () {
+            // Set Toastr options
+            toastr.options = {
+                positionClass: 'toast-top-right',
+                closeButton: true,
+                progressBar: true,
+            };
 
-    @elseif(session() -> has('error'))
-
-    toastr.error('{{ session('
-        error ') }}', 'GAGAL!');
-
-    @endif
-
-</script>
-<!-- End Hero Section -->
-
-
-
+            // Display Toastr messages
+            @if(session()->has('success'))
+                toastr.success('{{ session('success') }}', 'BERHASIL!');
+            @elseif(session()->has('error'))
+                toastr.error('{{ session('error') }}', 'GAGAL!');
+            @endif
+        });
+    </script>
 @endsection
